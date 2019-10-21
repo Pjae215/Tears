b6ec1674ace1c2b42d0ae937646f2b5437d7591f
Pjae215 <lala2462002@hotmail.com>
2019-10-21 00:26:26 -0400

    added model associations

---
+++ app/models/likes.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, Sequelize) {
+ 
+    var Likes = sequelize.define('Likes', {
+ 
+        email: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+            validate: {
+                isEmail: true
+                }}
+    });
+
+    Likes.associate = function(models) {
+//no profile can exist without a user
+            Likes.hasOne(models.User, {
+              foreignkey: {
+                  allnowNull: false}
+            });
+          };
+    return Likes
+}
---
+++ app/models/profile.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, Sequelize) {
+ 
+    var Profile = sequelize.define('Profile', {
+ 
+        email: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+            validate: {
+                isEmail: true
+                }}
+    });
+
+    Profile.associate = function(models) {
+//no profile can exist without a user
+            Profile.hasOne(models.User, {
+              foreignkey: {
+                  allnowNull: false}
+            });
+//no profile can exist without preference since those are apart of the signup process..might be reaching here
+            Profile.hasOne(models.Prefs, {
+                foreignkey: {
+                allnowNull: false}
+              });
+          };
+    return Profile
+}
